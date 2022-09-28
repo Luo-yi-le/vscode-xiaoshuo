879ceffb6c16c2ae3c048f5a259ed996f089d745
lonely <wulingshan.luo@qq.com>
2022-09-28 14:40:32 +0800

    更换

--- src/reader/driver/biquge/index.ts
+++ src/reader/driver/biquge/index.ts
@@ -2,10 +2,8 @@ import * as cheerio from 'cheerio';
 import request from '../../../utils/request';
 import { TreeNode, defaultTreeNode } from '../../../explorer';
 import { ReaderDriver as ReaderDriverImplements } from '../../../@types';
-// import http = require('http');
-import { get  } from "https";
 
-const DOMAIN = 'https://www.sobiquge.com';
+const DOMAIN = 'http://www.ibiqu.net/modules/article/';
 const DOMAIN2 = 'https://wap.sobiquge.com';
 // https://www.bige7.com/
 class ReaderDriver implements ReaderDriverImplements {
@@ -20,12 +18,12 @@ class ReaderDriver implements ReaderDriverImplements {
   public async search(keyword: string): Promise<TreeNode[]> {
     const result: TreeNode[] = [];
     try {
-      const res = await request.send(DOMAIN2 + '/search.php?q=' + encodeURI(keyword));
+      const res = await request.send(DOMAIN2 + '/search.php?searchkey=' + encodeURI(keyword));
       const $ = cheerio.load(res.body);
-      $('.result-list .result-item.result-game-item').each(function (i: number, elem: any) {
+      $('.grid tr').each(function (i: number, elem: any) {
         const title = $(elem).find('a.result-game-item-title-link span').text();
         const author = $(elem).find('.result-game-item-info .result-game-item-info-tag:nth-child(1) span:nth-child(2)').text();
-        const path = $(elem).find('a.result-game-item-pic-link').attr().href;
+        const path = $(elem).find('a').attr().href;
         
         result.push(
           new TreeNode(
